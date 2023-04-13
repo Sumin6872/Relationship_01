36dfcfdb8160b7eb740781d1c3d17c4828c1ebfa
Sumin6872 <84171130+Sumin6872@users.noreply.github.com>
2023-04-14 00:30:19 +0900

    Update models.py

--- session/users/models.py
+++ session/users/models.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# AbstractBaseUser로 CustomUser 만들면 CustomUserManager로 관리
-# 커스텀하기 위해 UserManager을 사용하지 않고 BaseUserManager 사용
 class CustomUserManager(BaseUserManager):
-    # 필수 (Docs참고, Customizing authentication in Django)
-    def create_user(self, username, password, **kwargs):  # username, password, 기타(dict형)을 파라미터로 받음
+    
+    def create_user(self, username, password, **kwargs):
         user = self.model(username=username, **kwargs)
         user.set_password(password)
         user.save()
@@ -13,24 +11,21 @@ class CustomUserManager(BaseUserManager):
     def create_normaluser(self, username, password, **kwargs):
         self.create_user(username, password, **kwargs)
 
-    # 필수 (Docs참고, Customizing authentication in Django)
     def create_superuser(self, username, password, **kwargs):
         kwargs.setdefault("is_superuser", True)
         self.create_user(username, password, **kwargs)
 
 
-# AbstractBaseUser: Django가 기본 제공하는 User모델을 대체하기 위해 사용되는 추상 클래스
-# PermissionMixin: User 모델에 대한 권한을 부여하기 위해 사용되는 클래스
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    USERNAME_FIELD = 'username'  # 인증시 username 필드로 유저 식별
+    USERNAME_FIELD = 'username' 
 
     username = models.CharField(unique=True, max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    objects = CustomUserManager()  # CustomUser는 CustomUserManager를 사용함
+    objects = CustomUserManager()  
 
-    @property  # is_staff를 속성처럼 사용
     def is_staff(self):
         return self.is_superuser
 
